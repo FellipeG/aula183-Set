fa2a497e4aede1a1606fc2153476f0b05c87a3bd
FellipeG <fellipegarciasrj@yahoo.com.br>
2018-12-26 14:43:26 -0200

    Part 2

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,6 @@
 package application;
 
-import java.util.HashSet;
-import java.util.LinkedHashSet;
+import java.util.Arrays;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -9,59 +8,23 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println();
-		System.out.println("HashSet"); // Have no order
-		System.out.println();
+		Set<Integer> a = new TreeSet<>(Arrays.asList(0, 2, 4, 5, 6, 8, 10));
+		Set<Integer> b = new TreeSet<>(Arrays.asList(5, 6, 7, 8, 9, 10));
+		
+		// Union
+		Set<Integer> c = new TreeSet<>(a);
+		c.addAll(b);
+		System.out.println(c);
 
-		Set<String> set = new HashSet<>();
-		set.add("Tv");
-		set.add("Tablet");
-		set.add("Notebook");
+		// Intersection
+		Set<Integer> d = new TreeSet<>(a);
+		d.retainAll(b);
+		System.out.println(d);
 		
-		System.out.println(set.contains("Notebook"));
-		
-		for(String p : set) {
-			System.out.println(p);
-		}
-		
-		System.out.println();
-		System.out.println("TreeSet"); // Have an order and it's specified in CompareTo or Comparator
-		System.out.println();
-		
-		Set<String> set2 = new TreeSet<>();
-		set2.add("Tv");
-		set2.add("Notebook");
-		set2.add("Tablet");
-		
-		System.out.println(set2.contains("Notebook"));
-		
-		set2.removeIf(x -> x.length() >= 3);
-		
-		for(String p : set2) {
-			System.out.println(p);
-		}
-		
-		
-		System.out.println();
-		System.out.println("LinkedHashSet"); // Have an order, its's the order of insertion
-		System.out.println();
-		
-		
-		Set<String> set3 = new LinkedHashSet<>();
-		set3.add("Tv");
-		set3.add("Notebook");
-		set3.add("Tablet");
-		
-		System.out.println(set3.contains("Notebook"));
-		
-		
-		set3.remove("Tablet");
-		
-		for(String p : set3) {
-			System.out.println(p);
-		}
-		
-
+		// Difference
+		Set<Integer> e = new TreeSet<>(a);
+		e.removeAll(b);
+		System.out.println(e);
 	}
 
 }
